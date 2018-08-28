42b2da67a55ce7467fbd5430d30f501015a1442f
eugen neagoe <eneagoe@gmail.com>
2018-08-28 17:07:47 +0300

    Fix FactoryBot deprecation warnings

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
 
   factory :user do
     email { Faker::Internet.unique.safe_email }
-    password '12345678'
-    password_confirmation '12345678'
+    password { '12345678' }
+    password_confirmation { '12345678' }
   end
 end
