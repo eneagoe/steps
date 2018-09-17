545985aacfbd515ed5860d5e2dcebf4f5db33eb5
eugen neagoe <eneagoe@gmail.com>
2018-09-17 13:05:40 +0300

    Do not allow records for future dates

--- app/models/day_log.rb
+++ app/models/day_log.rb
@@ -1,4 +1,16 @@
 # models the daily number of steps
 class DayLog < ApplicationRecord
   belongs_to :user
+
+  validates :taken_on, presence: true
+  validate :taken_in_the_past
+
+  private
+
+  def taken_in_the_past
+    return if taken_on.present? && taken_on <= Time.zone.today
+
+    i18n_scope = 'activerecord.errors.models.daily_log.attributes.taken_on'
+    errors[:taken_on] << I18n.t('in_the_past', scope: i18n_scope)
+  end
 end
--- spec/models/day_log_spec.rb
+++ spec/models/day_log_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe DayLog, type: :model do
     it 'is valid' do
       expect(log).to be_valid
     end
+
+    it 'should not save a record for the future' do
+      log.taken_on = Time.zone.tomorrow
+
+      expect(log).to be_invalid
+      expect(log.errors[:taken_on]).to_not be_empty
+    end
   end
 end
