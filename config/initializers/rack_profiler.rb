529d3fbbb8b5fafb0e21dff272bba3057f2d20b7
eugen neagoe <eneagoe@gmail.com>
2022-08-11 13:24:03 +0300

    Show rack-mini-profiler in top left

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,7 +1,9 @@
 if Rails.env.development?
   require 'rack-mini-profiler'
 
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+  Rack::MiniProfiler.config.position = 'top-left'
+
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
-  Rack::MiniProfiler.config.position = 'right'
 end
